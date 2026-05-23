4c9d965f4f1afae46ca14603e3d4758aec388e64
Simone-R0 <rinarellisimone@protonmail.com>
2026-05-23 17:09:13 +0200

    Update oil.lua inverted keybinding . and -

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -67,13 +67,13 @@ return {
       -- See :help oil-actions for a list of all available actions
       keymaps = {
         ["g?"] = "actions.show_help",
-        ["."] = "actions.select", --<CR> ["<C-s>"] = "actions.select_vsplit",
+        ["-"] = "actions.select", --<CR> ["<C-s>"] = "actions.select_vsplit",
         ["<C-h>"] = "actions.select_split",
         ["<C-t>"] = "actions.select_tab",
         ["<C-p>"] = "actions.preview",
         ["<C-c>"] = "actions.close",
         ["<C-l>"] = "actions.refresh",
-        ["-"] = "actions.parent",
+        ["."] = "actions.parent",
         ["_"] = "actions.open_cwd",
         ["`"] = "actions.cd",
         ["~"] = "actions.tcd",
